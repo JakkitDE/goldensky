866ec298f25d11d7dc6a309d3be6ec141b32d521
Tom Stahlberg <tom.stahlberg@jakkit.de>
2022-05-11 16:49:20 +0200

    Veel success

---
+++ javascript.js
@@ -0,0 +1,32 @@
+document.cookie.replace(/(?<=^|;).+?(?=\=|;|$)/g, name => location.hostname.split('.').reverse().reduce(domain => (domain=domain.replace(/^\.?[^.]+/, ''),document.cookie=`${name}=;max-age=0;path=/;domain=${domain}`,domain), location.hostname));
+
+/* Cookies */
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+if(getCookie("popupShown") != "true"){
+    document.getElementById('popup').style.visibility = "visible";
+}
+document.getElementById('cookiePopupId').addEventListener("click", function(){
+    setCookie("popupShown", "true", "360");
+    document.getElementById('popup').style.visibility = "hidden";
+});
